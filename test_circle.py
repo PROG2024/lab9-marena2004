a2bc50d7da949e0cb1e0110e10e69eca9cf13ec8
suparak <suparak.may@gmail.com>
2024-03-12 18:30:09 +0700

    update test

--- test_circle.py
+++ test_circle.py
@@ -15,14 +15,23 @@ from circle import Circle
 class TestCircle(unittest.TestCase):
     """ Tests of Circle class"""
 
-    def test_positive_add_area(self):
-        circle1 = Circle(5)
-        circle2 = Circle(7)
+    def set_up(self):
+        """set up for test circle"""
 
+    def test_positive_add_area(self):
+        """Test add_area with two circles having positive radii."""
+        self.circle1 = Circle(3)
+        self.circle2 = Circle(4)
+        combined_circle = self.circle1.add_area(self.circle2)
+        self.assertEqual(combined_circle.get_radius(), 5.0)
+        self.assertAlmostEqual(combined_circle.get_area(), 78.53981633974483)
 
     def test_zero_radius(self):
-        pass
-        # self.assertEqual(Circle.radius(), 0)
+        self.circle1 = Circle(0)
+        self.circle2 = Circle(6)
+        combined_circle = self.circle1.add_area(self.circle2)
+        self.assertEqual(combined_circle.get_radius(), 6.0)
+        self.assertAlmostEqual(combined_circle.get_area(), 113.09733552923255)
 
     def test_circle_constructor(self):
         with self.assertRaises(ValueError):
