2a2b24814be0aecf5b628e63fe0842785dcab4e1
suparak <suparak.may@gmail.com>
2024-03-13 14:43:46 +0700

    add docstring

--- counter/test_counter.py
+++ counter/test_counter.py
@@ -13,19 +13,25 @@ from counter import Counter
 
 
 class TestCounter(unittest.TestCase):
+    """Test of Counter class"""
 
     def setUp(self):
+        """ set up for Counter class"""
         self.counter1 = Counter()
         self.counter2 = Counter()
 
     def test_share_same_count(self):
+        """Test for verify that all instances share the same count."""
         self.counter1.increment()
         self.assertEqual(self.counter1, self.counter2)
 
     def test_counter_is_singleton(self):
+        """Test for verify that Counter is a singleton."""
         self.assertIs(self.counter1, self.counter2)
 
     def test_not_reset_to_zero(self):
+        """Test for the count is not reset to 0 when invoke
+           count = Counter() after the first time."""
         self.counter1.increment()
         new_counter = Counter()
         self.assertEqual(self.counter1.count, new_counter.count)
